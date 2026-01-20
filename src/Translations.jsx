a6f99a41d020690190375742626b22e6e741bcf8
Sanchiks07 <ipb23.s.mitniece@vtdt.edu.lv>
2026-01-20 12:47:09 +0200

    uzlaboju tulkojumus

--- src/Translations.jsx
+++ src/Translations.jsx
@@ -2,17 +2,18 @@ import './Translations.css'
 import GherkinTable from './GherkinTable.jsx'
 
 const gherkinTerms = [
-    { en: "Feature", lv: "Funkcija" },
+    { en: "Feature", lv: "Funkcionālā iezīme" },
     { en: "Background", lv: "Konteksts" },
     { en: "Rule", lv: "Noteikums / Nosacījums" },
     { en: "Scenario", lv: "Scenārijs" },
-    { en: "Scenario Outline", lv: "Scenārija Piemērs" },
+    { en: "Scenario Outline", lv: "Scenārijs pēc parauga" },
     { en: "Examples", lv: "Piemēri" },
-    { en: "Given", lv: "Dots" },
-    { en: "When", lv: "Kad" },
+    { en: "Given", lv: "Kad" },
+    { en: "When", lv: "Ja" },
     { en: "Then", lv: "Tad" },
     { en: "And", lv: "Un" },
     { en: "But", lv: "Bet" },
+    { en: "Or", lv: "Vai" },
 ];
 
 function Translations() {
